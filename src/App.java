2cea08b52f9cbcfa6fa5fdc9dc9c65535c8a922b
Makima32 <omartorresyuste@gmail.com>
2025-11-03 23:39:24 +0000

    version final

--- src/App.java
+++ src/App.java
@@ -3,24 +3,25 @@ import java.util.Scanner;
 
 public class App {
 
-    public void pedirDatos() {
-        
-    }
-
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
 
-         int NumeroApostadores = 2;
-         String modoDeJuego = "numeroconcreto"; // numeroConcreto || pares || martingala
+        int NumeroApostadores = 2;
+        String modoDeJuego = "martingala"; // numeroconcreto || pares || martingala
         int numeroApuesta;
         Apostador[] apostadores = new Apostador[NumeroApostadores];
         Thread[] hilos = new Thread[NumeroApostadores];
 
         Casino casino = new Casino();
-
-        boolean numeroCorrecto = false;
         
+        // Comprobar que el modo de juego sea el valido
+        if (!((modoDeJuego.equals("numeroconcreto")) || (modoDeJuego.equals("pares"))
+                || (modoDeJuego.equals("martingala")))) {
+            System.out.println("Eliga un modo de juego valido");
+            System.exit(1);
+        }
+
         casino.AnuncioJuego(modoDeJuego);
 
         for (int i = 0; i < NumeroApostadores; i++) {
@@ -28,28 +29,27 @@ public class App {
             System.out.println("Nombre para el apostador " + (i + 1));
             String nombreApostador = scanner.nextLine();
 
-            System.out.println("Numero a apostar para el apostador " + nombreApostador);
+            try {
 
-            
-          
-                
-          
-                try {
-
-                    numeroApuesta = scanner.nextInt();
-                    scanner.nextLine();
-                    apostadores[i] = new Apostador(nombreApostador, numeroApuesta, casino, modoDeJuego);
+                System.out.println("Numero a apostar para el apostador " + nombreApostador);
+                numeroApuesta = scanner.nextInt();
+                if (numeroApuesta > 36) {
                     
-                    // Comprobar que mete un numero entero
-                } catch (InputMismatchException e) {
-
-                    System.out.println("\n ---------- ERROR ----------");
-                    System.out.println("  Elige un puto numero y aprende a leer ");
-
+                    System.out.println("Elige un numero entre 1 y 36");
+                    break;
                 }
+                scanner.nextLine();
+                apostadores[i] = new Apostador(nombreApostador, numeroApuesta, casino, modoDeJuego);
 
-      
-                    
+                // Comprobar que mete un numero entero
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("\n ---------- ERROR ----------");
+                System.out.println("  Elige un puto numero y aprende a leer ");
+                break;
+            }
+
         }
 
         scanner.close();
@@ -63,4 +63,5 @@ public class App {
         }
 
     }
+
 }
